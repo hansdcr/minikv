3129ae8b2bb7b67d9c6675d78c3bdad7b049c45f
hans <464501551@qq.com>
2022-06-18 20:59:13 +0800

    测试

---
+++ linkedlist/Node.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Node struct {
+	data int
+	next *Node
+}
+
+func SingleLinkedList() {
+
+}
+
+func (node Node) Search(data int) {
+
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
